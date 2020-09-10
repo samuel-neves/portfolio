a51f87da4564d51cf2869b315ba84dc2f76b7766
Samuel212584 <samuel212584@gmail.com>
2020-09-10 09:05:19 -0300

    formating project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { Container } from './styles';
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code> src/App.tsx </code>
-        and save to reload. Hello there!
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <Container>
+    <h1>Hello world</h1>
+  </Container>
 );
 
 export default App;
