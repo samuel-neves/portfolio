93ff3ce0c510bf14f157a88e20603d86b2ba66a9
Samuel Neves <sneves2125@gmail.com>
2022-03-24 23:26:44 +0000

    style: adding assets and exporting as component

---
+++ assets/index.ts
@@ -0,0 +1,29 @@
+import CubeMissingTopLeftPart from './Asset Rect.png';
+import BackgroundTexture from './background-texture.png';
+import SemiCircleTexture from './semicircle-texture.png';
+import WeirdShapeTexture from './weirdshape-texture.png';
+import RoundedCube from './Cube_Super_Rounded0001 1.png';
+import EggShapeAsset from './Egg-Shapes.png';
+import GreenArrowAsset from './Green-Arrow.png';
+import OrangeArrowAsset from './Orange-Arrow.png';
+import PurpleSphere from './Sphere0006.png';
+import SmallTubeSlice from './Tube_Short_X2_-_Thin0006.png';
+import DeformedGreenSphere from './deformed-sphere.png';
+import GitSymbol from './git-symbol.png';
+import CodeSymbol from './code-symbol.png';
+
+export {
+  CubeMissingTopLeftPart,
+  BackgroundTexture,
+  SemiCircleTexture,
+  WeirdShapeTexture,
+  RoundedCube,
+  EggShapeAsset,
+  GreenArrowAsset,
+  OrangeArrowAsset,
+  PurpleSphere,
+  SmallTubeSlice,
+  DeformedGreenSphere,
+  GitSymbol,
+  CodeSymbol,
+};
